a57d3b4e6db1928dc2f1a66ad22deb5b07d2d6de
Divyangi Mahajan <divyangi.mahajan17@gmail.com>
2023-08-23 18:38:46 +0530

    Added palindrome code

--- Palindrome.js
+++ Palindrome.js
@@ -1,1 +1,36 @@
 // check if string is palindrome
+
+const checkPalindrome = (value) => {
+    if (value.split('').reverse().join('') === value)
+        return true;
+    return false;
+}
+
+const checkPalindromeIterator = (value) => {
+    const val = value.split('')
+    for (let i = 0; i < value.length; i++) {
+        let length = value.length - 1;
+
+        if (value.charAt(i) !== value.charAt(length - i)) {
+            return false
+        }
+    }
+    return true;
+}
+
+console.log(checkPalindrome('abccba'))
+console.log(checkPalindrome('1234321'))
+console.log(checkPalindrome('ertt'))
+
+console.log(checkPalindromeIterator('abccba'))
+console.log(checkPalindromeIterator('1234321'))
+console.log(checkPalindromeIterator('ertt'))
+
+// OUTPUT
+// true
+// true
+// false
+
+// true
+// true
+// false
